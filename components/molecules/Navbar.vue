99b1a5d3f3e97e2304d5787d184dea9e731b8f21
claranceliberi <liberintwari@gmail.com>
2022-03-29 11:43:46 +0200

    feat: navbar

--- components/molecules/Navbar.vue
+++ components/molecules/Navbar.vue
@@ -1,5 +1,16 @@
 <template>
-    <div>
-        <atoms-logo />
+    <div class="flex justify-between mx-2">
+        <div>
+            <atoms-logo />
+        </div>
+        <ul class="list-none links">
+            <li >
+                <router-link class="no-underline " to="/about">About</router-link>
+            </li>
+        </ul>
     </div>
 </template>
+
+<style lang="scss" scoped>
+
+</style>
