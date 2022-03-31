efba575c97910c6211519a1e177c160855abfadd
claranceliberi <liberintwari@gmail.com>
2022-03-31 20:25:54 +0200

    feat: new intro

--- pages/index.vue
+++ pages/index.vue
@@ -4,12 +4,13 @@
             <molecules-navbar />
         </header>
         <section class="flex items-center justify-center py-8 text-lg">
-            <article class="w-fit">
+            <article class="w-fit italic">
                 <p>Hello,</p>
                 <div class="py-2 flex items-baseline">
                     <p class="pr-2">I'm</p>
                     <h3 class="font-bold text-xl">Ntwari Clarance Liberiste</h3>
                 </div>
+                <p>I <span>❤️</span> Software Engineering</p>
                 <p>Nice to meet you</p>
             </article>
         </section>
