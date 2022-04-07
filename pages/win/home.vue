4f4bf083e2707676e35fa39afa960910f58a355f
claranceliberi <liberintwari@gmail.com>
2022-04-07 15:42:08 +0200

    feat: custom assets

--- pages/win/home.vue
+++ pages/win/home.vue
@@ -1,1 +1,24 @@
-<template>windows home page</template>
+<template>
+    <div class="window flex flex-col">
+        <div class="home-view"></div>
+        <div class="button-bar bg-theme-darker">so</div>
+    </div>
+</template>
+
+<style lang="scss">
+.window {
+    height: 100vh;
+    .home-view {
+        width: 100vw;
+        height: 100%;
+        background-image: url("../../assets/images/home.jpg");
+        background-position: center;
+        background-repeat: no-repeat;
+        background-size: cover;
+    }
+    .button-bar {
+        height: 3rem;
+        width: 100vw;
+    }
+}
+</style>
